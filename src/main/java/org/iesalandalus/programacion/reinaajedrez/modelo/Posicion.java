769ce675e1221260897e28fe5bd6a7fe96b0c225
andre <andre@LAPTOP-2F3QRC2C>
2022-11-14 14:30:12 +0100

    commit posicion hasta enunciado 6 incluido

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
 	private int fila;
 	private char columna;
@@ -41,6 +43,32 @@ public class Posicion {
 			throw new IllegalArgumentException("la columna es incorrecta");
 		}
 	}
+	
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+
+	@Override
+	public String toString() {
+		return "Posicion [fila=" + fila + ", columna=" + columna + "]";
+	}
+	
+	
 	
 	
 	

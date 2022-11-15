1819273cc6bf11a380d18bec62a46c53cf84bc19
andre <andre@LAPTOP-2F3QRC2C>
2022-11-15 14:35:21 +0100

    commit clase reina hasta enunciado 3(incluido)

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -5,12 +5,34 @@ public class Reina {
 	public Posicion posicion;
 	
 	
+	public Reina(){
+		color = color.BLANCO;
+		posicion = new Posicion(1, 'd');
+	
+	}
+
+	
+	
+	public Reina(Color color, Posicion posicion){
+		
+		this.color = color;
+		this.posicion = posicion;
+		
+		
+	}
+	
+	
 	public Color getColor() {
 		return color;
 	}
 	
 	
 	private void setColor(Color color) {
+		if (posicion == null) {
+			throw new NullPointerException ("ERROR: No es posible copiar una posición nula.");
+				
+		}
+		
 		this.color = color;
 	}
 	
@@ -21,6 +43,11 @@ public class Reina {
 	
 	
 	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException ("ERROR: No es posible copiar un color nulo.");
+				
+		}
+		
 		this.posicion = posicion;
 	}
 	

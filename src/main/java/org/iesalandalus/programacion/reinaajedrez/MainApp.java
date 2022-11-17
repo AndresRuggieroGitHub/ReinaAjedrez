d74dfb67582344aacb9da536d5f87abea2ebeec9
andre <andre@LAPTOP-2F3QRC2C>
2022-11-17 12:30:14 +0100

    commit final

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,31 +1,74 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
 	
 	public static void main(String[] args) {
 		
+		int opcion;
 		
-		
-		private ejecutarOpcion(int opcion) {
+		do {
+			Consola.mostrarMenu();
+			opcion= Consola.elegirOpcionMenu();
 			
-		}
-		
-		private crearReinaDefecto() {
+			MainApp.ejecutarOpcion(opcion);
 			
-		}
-		
-		private crearReinaColor() {
-			
-		}
-		
-		private mover() {
-			
-		}
-		private mostrarReina() {
-			
-		}
-		
-		
+		}while(opcion != 4);
 		
 	}
+		private static Reina reina;
+		
+		
+		private static void ejecutarOpcion(int opcion) {
+			
+			if(opcion == 1 ) {
+				MainApp.crearReinaDefecto();
+				MainApp.mostrarReina();
+			}
+			
+			
+			if(opcion == 2)
+			{
+				MainApp.crearReinaColor();
+				MainApp.mostrarReina();
+			}
+			
+			if(opcion == 3) {
+				MainApp.moverReina();
+				MainApp.mostrarReina();
+			}
+			
+			if(opcion == 4) {
+				Consola.despedirse();
+			}
+		}
+		
+		
+		private static void crearReinaDefecto() {
+			reina = new Reina();
+		}
+		
+		private static void crearReinaColor() {
+			reina = new Reina(Consola.elegirColor());
+			
+		}
+		
+		private static void moverReina() {
+			try {
+				Consola.mostrarMenuDirecciones();
+				reina.mover(Consola.elegirDireccion(), Consola.elegirPasos());
+			}catch(OperationNotSupportedException e) {
+				e.printStackTrace();
+			}
+		}
+		private static void mostrarReina() {
+			System.out.println(reina.toString());
+		}
+		
+		
+		
+	
 }
